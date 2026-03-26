9ac0e76cb451f1cf33c9deed0ea061c2c25b780c
francesco <francesco-stacks@users.noreply.github.com>
2026-03-26 10:17:18 +0000

    remove debug print

--- stackslib/src/chainstate/burn/db/sortdb.rs
+++ stackslib/src/chainstate/burn/db/sortdb.rs
@@ -3437,7 +3437,6 @@ impl SortitionDB {
     fn check_schema_version_or_error(&mut self) -> Result<(), db_error> {
         match SortitionDB::get_schema_version(self.conn()) {
             Ok(Some(version)) => {
-                debug!("SortitionDB schema version: {version}");
                 if version == SORTITION_DB_VERSION {
                     Ok(())
                 } else {
